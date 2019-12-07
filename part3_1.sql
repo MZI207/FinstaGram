742a1169fb1b2d25d2a84f612896ee3e81359eb3
MZI207 <32904221+MZI207@users.noreply.github.com>
2019-12-07 18:20:25 -0500

    Delete part3_1.sql

--- part3_1.sql
+++
@@ -1,6 +0,0 @@
-SELECT DISTINCT photoID FROM photo WHERE photoPoster in (
-	SELECT username_followed FROM follow WHERE username_follower="Isiah" AND followstatus=1
-	)
-OR photoID in (
-	SELECT photoID FROM sharedwith NATURAL JOIN belongto WHERE member_username="Isiah"
-	)
